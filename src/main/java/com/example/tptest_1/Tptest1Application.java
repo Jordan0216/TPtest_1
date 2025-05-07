db583d8f3f111f42610358741221556c507fddc8
Jordan <jordandan216@gmail.com>
2025-05-07 11:30:32 +0900

    initial commit

---
+++ src/main/java/com/example/tptest_1/Tptest1Application.java
@@ -0,0 +1,13 @@
+package com.example.tptest_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptest1Application.class, args);
+	}
+
+}
